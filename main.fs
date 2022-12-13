2dd0894c730c4dde0c0f6864dd3d5af47321dba0
Mark Harmer <drivehappy@gmail.com>
2022-12-12 22:45:32 -0800

    Problem 13

--- main.fs
+++ main.fs
@@ -2,137 +2,186 @@
 
 open System.IO
 
-// !!!!
-let monkeyCount = 7
-let f = File.ReadAllLines("../../../input11.txt")
-//
+let f = File.ReadAllLines("../../../input13.txt")
+
+type PacketData =
+    | Int of int
+    | List of PacketData list
+    | Empty
 
 
-let mutable monkeyItems: int64 list array = Array.zeroCreate (monkeyCount + 1)
-for i in 0..monkeyCount do
-    monkeyItems[i] <- []
+let rec parse (s: string) (j: int) : (PacketData * int) =
+    //let chars = s.ToCharArray()
 
-// Parse initial items
-let mutable line = 0
-let mutable index = 0
-for i in 0..monkeyCount do
-    let items =
-        f[line+1].Substring(18).Split(',')
-        |> Array.map (fun s -> s.Trim() |> int64)
-        |> Array.toList
+    let mutable level = 0
+    let mutable data : int list = []
+    
+    let mutable i = j
+    let mutable loop = true
 
-    monkeyItems[index] <- items
-    printfn "Items[%i]: %A" index monkeyItems[index]
+    let packet =
+        [
+            while loop && i < s.Length do
+                match s[i] with
+                | '[' ->
+                    let (p, ni) = parse s (i+1)
+                    i <- ni-1
+                    yield p
+                | ']' ->
+                    // Exit loop
+                    loop <- false
+                | ',' -> ()
+                | c ->
+                    let a = i
+                    while i < s.Length && s[i] <> '[' && s[i] <> ']' && s[i] <> ',' do
+                        i <- i + 1
+                    i <- i-1
 
-    index <- index + 1
-    line <- line + 7
+                    yield Int (int (s.Substring(a, i-a+1)))
 
+                i <- i + 1
+        ]
+        |> List
 
-// Old -> New
-let monkeyOp (monkey: int) (old: int64) : int64 =
-    match monkey with
-    | 0 -> old * 7L
-    | 1 -> old + 8L
-    | 2 -> old * 13L
-    | 3 -> old + 7L
-    | 4 -> old + 2L
-    | 5 -> old + 1L
-    | 6 -> old + 4L
-    | 7 ->
-        let n = old * old
-        if n > 9699690L then
-            //printfn "DEBUG: %A" n
-            n % 9699690L
-        else n
-    | _ -> assert(false); 0
+    (packet, i)
 
 
-(*
-let monkeyOp (monkey: int) (old: WorryLevel) : WorryLevel =
-    match monkey with
-    | 0 -> { old with worry = old.worry * 19L }
-    | 1 -> { old with worry = old.worry + 6L }
-    | 2 -> 
-        let n = old.worry * old.worry
-        (*
-        if n % 96577L = 0 then
-            printfn "DEBUG: %A" n
-            { worry = n / 96577L; multiplier = old.multiplier + 1L }
-        *)
-        if n > 96577L then
-            //printfn "DEBUG: %A" n
-            { worry = n % 96577L; multiplier = old.multiplier + 1L }
-        else { old with worry = n }
-    | 3 -> { old with worry = old.worry + 3L }
-    | _ -> assert(false); { old with worry = 0 }
-*)
-
-// Test: item -> monkeyIndex
-let monkeyTest (srcMonkey: int) (worry: int64) : int =
-    match srcMonkey with
-    | 0 -> if worry % 17L = 0L then 5 else 3
-    | 1 -> if worry % 2L = 0L then 7 else 6
-    | 2 -> if worry % 5L = 0L then 1 else 6
-    | 3 -> if worry % 3L = 0L then 5 else 2
-    | 4 -> if worry % 7L = 0L then 0 else 3
-    | 5 -> if worry % 13L = 0L then 2 else 1
-    | 6 -> if worry % 19L = 0L then 7 else 4
-    | 7 -> if worry % 11L = 0L then 0 else 4
-    | _ -> assert(false); 0
-
+let parsed =
+    f
+    |> Array.choose (fun x -> 
+        if x.Length = 0 then None
+        else parse (x.Substring(1, x.Length-2)) 0 |> fst |> Some
+    )
 
 (*
-let monkeyTest (srcMonkey: int) (worry: WorryLevel) : int =
-    match srcMonkey with
-    | 0 -> if worry.worry % 23L = 0 then 2 else 3
-    | 1 -> if worry.worry % 19L = 0 then 2 else 0
-    | 2 -> if worry.worry % 13L = 0 then 1 else 3
-    | 3 -> if worry.worry % 17L = 0 then 0 else 1
-    | _ -> assert(false); 0
+printfn "Parsed:"
+parsed
+|> Array.iter (fun p ->
+    printfn "  %A" p
+    printfn ""
+)
 *)
 
-//
-let mutable monkeyInspect: int array = Array.zeroCreate (monkeyCount + 1)
-for i in 0..monkeyCount do
-    monkeyInspect[i] <- 0
+// Rules
 
-for round in 1..10000 do
-    //printfn "Round %i" round
+type Ordering =
+    | Ordered
+    | NotOrdered
+    | Unknown
 
-    for monkeyIndex in 0..monkeyCount do
-        //printfn "monkeyIndex: %A" monkeyIndex
+let rec checkPacket (l: PacketData) (r: PacketData) : Ordering =
+    match (l, r) with
+    | (Empty, Empty) -> Unknown
+    | (Empty, _) -> Ordered
+    | (_, Empty) -> NotOrdered
+    | (Int li, Int ri) -> if li < ri then Ordered elif li > ri then NotOrdered else Unknown
+    | (List ll, List rl) ->
+        if List.length ll = 0 && List.length rl = 0 then Unknown
+        elif List.length ll = 0 then Ordered
+        elif List.length rl = 0 then NotOrdered
+        else
+            // Adjust lengths
+            let diff = List.length ll - List.length rl
 
-        monkeyItems[monkeyIndex]
-        |> List.iter (fun worryItem ->
-            monkeyInspect[monkeyIndex] <- monkeyInspect[monkeyIndex] + 1
+            let newL =
+                if diff < 0 then
+                    [
+                        for i in 1..abs(diff) do
+                            yield Empty
+                    ]
+                    |> List.append ll
+                else ll
 
-            //printfn "  worryItem: %A" worryItem
-            let worryLevel = monkeyOp monkeyIndex worryItem
-            //let boredLevel: int64 = worryLevel / 3L
+            let newR =
+                if diff > 0 then
+                    [
+                        for i in 1..abs(diff) do
+                            yield Empty
+                    ]
+                    |> List.append rl
+                else rl
+              
+            //
+            let l = List.zip newL newR
+            let lo =
+                l
+                |> List.map (fun (l, r) ->
+                    //printfn "D1: %A %A" l r
+                    let o = checkPacket l r
+                    o
+                )
 
-            let boredLevel: int64 =
-                (*
-                if worryLevel.worry % 96577L = 0 then
-                    printfn "DEBUG: %A" worryLevel.worry
-                    { worryLevel with worry = worryLevel.worry / 96577L; multiplier = worryLevel.multiplier + 1L }
-                *)
-                if worryLevel > 9699690L then
-                    worryLevel % 9699690L
-                else worryLevel
+            //printfn ""
+            //printfn "%A" l
+            //printfn "%A" lo
 
-            let newMonkey = monkeyTest monkeyIndex boredLevel
-            //printfn "  Throw: %i -> %i (%i -> %i)" monkeyIndex newMonkey worryItem boredLevel
+            let loo =
+                lo
+                |> List.fold (fun s t -> if s = Unknown then t else s) Unknown
 
-            // Add item
-            monkeyItems[newMonkey] <- (boredLevel :: (monkeyItems[newMonkey] |> List.rev)) |> List.rev
-        )
+            //printfn "%A" loo
 
-        // Clear old monkey items
-        monkeyItems[monkeyIndex] <- []
+            //if lo = Unknown then Ordered else lo
+            loo
+    | (Int li, List rl) -> checkPacket (List [Int li]) r
+    | (List ll, Int ri) -> checkPacket l (List [Int ri])
 
 
-//printfn "Items: %A" monkeyItems
+let input =
+    parsed
+    |> Array.chunkBySize 2
+    |> Array.map (fun p -> (p[0], p[1]))
 
-let s = monkeyInspect |> Array.sortDescending
-printfn "Results: %A" s
-printfn "Soln1: %A" ((bigint s[0]) * (bigint s[1]))
+let pairs =
+    input
+    |> Array.map (fun (l, r) ->
+        //printfn ""
+
+        let o = checkPacket l r
+        //printfn "O: %A" o
+        o
+    )
+    //|> Array.map (fun o -> if o = Unknown then Ordered else o)
+
+(*
+printfn "Order:"
+pairs
+|> Array.iter (fun p ->
+    printfn "  %A" p
+)
+*)
+
+let soln1 =
+    pairs
+    |> Array.indexed
+    |> Array.filter (fun (i, o) ->
+        o = Ordered
+    )
+    |> Array.sumBy (fun (i, _) -> i+1)
+
+printfn "Soln1: %A" soln1
+
+
+// Sort
+let sorted =
+    parsed
+    |> Array.sortWith (fun l r ->
+        if checkPacket l r = Ordered then -1
+        else 1
+    )
+
+(*
+printfn "S"
+sorted
+|> Array.iter (fun a -> printfn "  %A" a)
+*)
+
+let soln2 =
+    sorted
+    |> Array.indexed
+    |> Array.choose (fun (i, p) ->
+        if p = List [ List [ Int 6 ] ] || p = List [ List [ Int 2 ] ] then Some (i+1)
+        else None
+    )
+
+printfn "%A" (soln2[0] * soln2[1])
